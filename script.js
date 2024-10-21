f040937b7feb96dbfe6d44d79192438e4161c46c
HoloKitab <holokitab@gmail.com>
2024-10-21 18:02:10 +0530

    changes
    changes

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript to handle smooth scrolling
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
